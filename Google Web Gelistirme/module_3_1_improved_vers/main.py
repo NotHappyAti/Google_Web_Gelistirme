2be7fc96d8072555df1a830f773bff92b2c69e66
NotHappyAti <atakanemir.caldag@gmail.com>
2025-01-18 04:48:51 +0300

    Final updates, CRUD completed.

--- Google Web Gelistirme/module_3_1_improved_vers/main.py
+++ Google Web Gelistirme/module_3_1_improved_vers/main.py
@@ -6,14 +6,14 @@ from typing import Optional, List
 app = FastAPI()
 
 class Course(BaseModel):
-    id: int
+    idnum: int
     instructor: str
     title: str
     rating: int
     publish_year: int
 
 class courseRequest(BaseModel):
-    id: Optional[int] = None
+    idnum: Optional[int] = None
     instructor: str = Field(min_length=3, max_length=50)
     title: str = Field(min_length=3, max_length=50)
     rating: int = Field(gte=0, lte=5)
@@ -31,12 +31,12 @@ class courseRequest(BaseModel):
     }
 
 courses_db = [
-    Course(id=1, instructor="John Doe", title="Python Programming", rating=4, publish_year=2023),
-    Course(id=2, instructor="Jane Doe", title="Java Programming", rating=3, publish_year=2021),
-    Course(id=3, instructor="Jim Doe", title="Data Science", rating=5, publish_year=2024),
-    Course(id=4, instructor="John Doe", title="Machine Learning", rating=4, publish_year=2020),
-    Course(id=5, instructor="Jack Doe", title="Web Development", rating=3, publish_year=2021),
-    Course(id=6, instructor="Jenny Doe", title="Android Development", rating=5, publish_year=2021),
+    Course(idnum=1, instructor="John Doe", title="Python Programming", rating=4, publish_year=2023),
+    Course(idnum=2, instructor="Jane Doe", title="Java Programming", rating=3, publish_year=2021),
+    Course(idnum=3, instructor="Jim Doe", title="Data Science", rating=5, publish_year=2024),
+    Course(idnum=4, instructor="John Doe", title="Machine Learning", rating=4, publish_year=2020),
+    Course(idnum=5, instructor="Jack Doe", title="Web Development", rating=3, publish_year=2021),
+    Course(idnum=6, instructor="Jenny Doe", title="Android Development", rating=5, publish_year=2021),
 ]
 
 # root path
@@ -46,7 +46,7 @@ async def get_all_courses():
 
 @app.get("/courses/{course_id}", status_code=status.HTTP_202_ACCEPTED)
 async def get_course_by_id(course_id: int = Path(gt=0)):
-    selected_course = [course for course in courses_db if course.id == course_id]
+    selected_course = [course for course in courses_db if course.idnum == course_id]
     if selected_course:
         return selected_course
     else:
@@ -62,10 +62,35 @@ async def get_course_by_rating(rating: int = Query(gt=0, lt=6)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Course not found.')
 
 @app.put("/courses/create_course", status_code=status.HTTP_201_CREATED)
-async def create_course(course: courseRequest):
-    course = find_id(course).model_dump()
+async def create_course(course_request: courseRequest):
+    course = course_request.model_dump()
+    course = Course(**find_id(course))
     courses_db.append(course)
+    return None
     
 def find_id(course):
-    course.id = 1 if len(courses_db) == 0 else courses_db[-1].id + 1
+    course['idnum'] = 1 if len(courses_db) == 0 else courses_db[-1].idnum + 1
     return course
+
+@app.put("/courses/update_course", status_code=status.HTTP_204_NO_CONTENT)
+async def update_course(course: courseRequest):
+    changed = False
+    for i in range(len(courses_db)):
+        if courses_db[i].idnum == course.idnum:
+            courses_db[i] = course
+            changed = True
+            break
+    if not changed:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, details= "Course not found.")
+    
+@app.delete("/courses/delete", status_code=status.HTTP_200_OK)
+async def delete_course(idnum: int):
+    deleted = False
+    for i in range(len(courses_db)):
+        if courses_db[i].idnum == idnum:
+            del courses_db[i]
+            deleted = True
+            break
+        
+    if not deleted:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Course not found or already removed.")
